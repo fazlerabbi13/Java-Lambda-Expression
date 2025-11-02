5b94e963d71a312981778ee6562db076eca82745
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-02 23:39:00 +0600

    Stream map(Function mapper) returns a stream consisting of the results of applying the given function to the elements of this stream.

---
+++ Map.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Map {
+    public static void main(String[] args)
+    {
+
+        System.out.println("The stream after applying "
+                           + "the function is : ");
+
+        // Creating a list of Integers
+        List<Integer> list = Arrays.asList(3, 6, 9, 12, 15);
+
+        // Using Stream map(Function mapper) and
+        // displaying the corresponding new stream
+        list.stream().map(number -> number * 3).forEach(System.out::println);
+    }
+}
